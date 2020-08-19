715a7ed1bbc8ca66625dea4711622228dfd777e1
Abhinav Raj <60300996+coderpalce@users.noreply.github.com>
2020-08-19 22:07:19 +0530

    Add files via upload

---
+++ Array Journay.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+void arrayJournay(int arr[], int k, int n){
+    int sum=arr[0];
+    int pos;
+    int i;
+    int count=0;
+    for(i=1;i<n;){
+            int m=arr[i];
+            pos=i;
+            count=0;
+        for(int j=1;j<k;j++){
+                if(i+j<n){
+                count++;
+                    if(arr[i+j]>m){
+                     m=arr[i+j];
+                     pos=i+j;
+                }
+            }
+        }
+        if(count==k-1){
+            sum=sum+m;
+            }
+        i=pos+1;
+    }
+    cout<<sum;
+
+}
+
+int main(){
+    int n;
+    int k;
+    cout<<"Enter array size: ";
+    cin>>n;
+    cout<<"\n Enter array journay size: ";
+    cin>>k;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        arr[i]=temp;
+    }
+        arrayJournay(arr,k,n);
+        return 0;
+
+
+}
